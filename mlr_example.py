9812cfb6467dcf1e201e368f8f95ed8cc78bf9a6
Daniel Pimentel Nunes <fc48995@alunos.fc.ul.pt>
2022-07-10 18:58:43 +0100

    Wrote function on mlr_example

--- mlr_example.py
+++ mlr_example.py
@@ -1,1 +1,10 @@
-#a basic program to check the functioning of the classes
+import numpy as np
+import dn_multiple_linear_regression
+import dn_scale_norm
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+model = dn_multiple_linear_regression.MLRegression()
+
+model.fit(X_train, y_train, 10, 0.001)
